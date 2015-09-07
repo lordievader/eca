6d59c5c4600ccb373916d5a3ac2c450ad15c3b64
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2015-09-07 11:28:48 +0200

    Slight optimization

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-// #include "Arduino.h"
-#include <string>
-
-=======
 #ifdef ARDUINO_TARGET
 #include "Arduino.h"
->>>>>>> master
 #define F_CPU 16000000
 #define ARDUINO 100
 #else
@@ -60,6 +54,12 @@ void printTime(int begin, int end)
     snprintf(message, 100, "There have been %d ticks since the beginning of this program.\n", diff);
     logMessage(string(message));
 }
+void printTime(int ticks)
+{
+    char message[100];
+    snprintf(message, 100, "There have been %d ticks on average.\n", ticks);
+    logMessage(string(message));
+}
 #endif
 
 void printMatrix(unsigned char matrix[rows][columns])
@@ -89,22 +89,23 @@ void printMatrix(unsigned char matrix[rows][columns])
 
 int calcValue(unsigned char matrix[rows][columns], int row, int column)
 {
-    int A[columns];
-    int B[rows];
-    for (int j = 0; j < columns; j++)
-    {
-        A[j] = matrix[row][j];
-    }
-
-    for (int j = 0; j < rows; j++)
-    {
-        B[j] = matrix[j][column];
-    }
+//     int A[columns];
+//     int B[rows];
+//     for (int j = 0; j < columns; j++)
+//     {
+//         A[j] = matrix[row][j];
+//     }
+//
+//     for (int j = 0; j < rows; j++)
+//     {
+//         B[j] = matrix[j][column];
+//     }
 
     int value = 0;
     for (int i = 0; i < rows; i++)
     {
-        value += A[i] * B[i];
+//         value += A[i] * B[i];
+        value += matrix[row][i] * matrix[i][column];
     }
     return value;
 }
@@ -134,11 +135,16 @@ void setup()
 
 void loop()
 {
-    int begin = getTime();
-    squareMatrix(A, B);
-    int end = getTime();
+    int avg = 0;
+    for (int i = 0; i <= 100000; i++)
+    {
+        int begin = getTime();
+        squareMatrix(A, B);
+        int end = getTime();
+        avg = (avg + (end - begin))/2;
+    }
     printMatrix(B);
-    printTime(begin, end);
+    printTime(avg);
     logMessage("DONE\n");
 #ifdef ARDUINO_TARGET
     while(1);
